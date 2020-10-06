d9e5c4cff6a2e5317926f9db4856556396c875f4
Fabricio Chalub <f.chalub@catawiki.nl>
2020-10-06 19:40:37 +0200

    Add unit tests for cliques, connected/strongly connected components

--- tests/main.lisp
+++ tests/main.lisp
@@ -23,6 +23,14 @@
     :g (:f)
     :h (:g :d)))
 
+(defvar *cliques*
+  '(:a (:b :e)
+    :b (:a :c :e)
+    :c (:d :b)
+    :d (:c :e :f)
+    :e (:a :b :d)
+    :f (:d)))
+
 (defun simple-bfs ()
   (let ((path nil))
     (bfs :a (lambda (n) (getf *simple-graph* n))
@@ -42,6 +50,33 @@
      (gethash to dist)
      (reconstruct-path prev to))))
 
+(defun complex-strongly-connected-components ()
+  (let ((sccs nil))
+    (strongly-connected-components
+     '(:a :b :c :d :e :f :g :h)
+     (lambda (n)
+       (getf *complex-graph* n))
+     (lambda (scc)
+       (push scc sccs)))
+    sccs))
+
+(defun complex-connected-components ()
+  (let ((ccs nil))
+    (connected-components
+     '(:a :b :c :d :e :f :g :h)
+     (lambda (n)
+       (getf *complex-graph* n))
+     (lambda (cc)
+       (push cc ccs)))
+    ccs))
+
+(defun cliques-maximal-cliques ()
+  (let ((cliques nil))
+    (maximal-cliques '(:a :b :c :d :e :f)
+                     (lambda (n) (getf *cliques* n))
+                     (lambda (n) (push n cliques)))
+    cliques))
+
 (test bfs
       (is (equal '(:a :b :c :d :f) (simple-bfs))))
 
@@ -56,3 +91,17 @@
       (is (equal '(2 (:b :f)) (complex-dijkstra :a :f)))
       (is (equal '(1 (:f)) (complex-dijkstra :b :f)))
       (is (equal '(4 (:b :c :d :h)) (complex-dijkstra :a :h))))
+
+(test maximal-cliques
+      (is (equal '((:d :e) (:d :f) (:c :d) (:b :c)
+                   (:e :a :b))
+                 (cliques-maximal-cliques))))
+
+(test strongly-connected-components
+      (is (equal '((:a :b :e)
+                   (:c :d :h)
+                   (:g :f))
+                 (complex-strongly-connected-components))))
+
+(test connected-components
+      (is (equal '(:a) (complex-connected-components))))
