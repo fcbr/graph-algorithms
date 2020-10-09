ca0b5ca1a4d8f54dc5495ad533a73737b388d151
Fabricio Chalub <f.chalub@catawiki.nl>
2020-10-09 16:10:54 +0200

    Use uppercase Q for queue, for consistency

--- graph-algorithms.lisp
+++ graph-algorithms.lisp
@@ -38,17 +38,17 @@ used as the start of the search.  NEIGHBORS-FN should return a list of
 immediate neighbor vertices of a given vertex.  VISITOR-FN is called
 on each new vertex found by the search. QUEUE-DEPH is the maximum
 queue depth used by CL-SPEEDY-QUEUE."
-  (let ((q (make-queue queue-depth))
+  (let ((Q (make-queue queue-depth))
         (discovered (make-hash-table)))
-    (enqueue source q)
+    (enqueue source Q)
     (setf (gethash source discovered) t)
-    (loop until (queue-empty-p q)
-       do (let ((p (dequeue q)))
+    (loop until (queue-empty-p Q)
+       do (let ((p (dequeue Q)))
             (funcall visitor-fn p)
             (dolist (v (funcall neighbors-fn p))
               (unless (gethash v discovered)
                 (setf (gethash v discovered) t)
-                (enqueue v q)))))))
+                (enqueue v Q)))))))
 
 (defun connected-components (vertices neighbors-fn visitor-fn)
   "VERTICES is the list of vertices of the graph. NEIGHBORS-FN should
